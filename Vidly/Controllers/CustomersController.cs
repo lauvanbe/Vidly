c114320685eea4287c12a6ee2c59b4450ce027e2
lauvanbe <lauvanbe@gmail.com>
2018-05-11 11:42:41 +0200

    exercice 1

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+ 
+        public ActionResult ViewCustomers()
+        {
+            var listCustomers = new List<Customer>()
+            {
+                new Customer(){ Name = "Laurent Van Beveren", Id = 0},
+                new Customer(){ Name = "Mennens Louis", Id = 1}
+            };
+
+            var viewModel = new CustomersCustomerViewModel()
+            {
+                listCustomers = listCustomers
+            };
+
+            return View(viewModel);
+        }
+
+        [Route("Customers/ViewCustomers/{id}")]
+        public ActionResult ViewDetail(int id)
+        {
+            var listCustomers = new List<Customer>()
+            {
+                new Customer(){ Name = "Laurent Van Beveren", Id = 0},
+                new Customer(){ Name = "Mennens Louis", Id = 1}
+            };
+
+            return View(listCustomers[id]);
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -12,12 +11,66 @@ namespace Vidly.Controllers
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() {Name = "Shrek!"};
-            return View(movie);
-//            return Content("Hello man!");
-//            return HttpNotFound();
-//            return new EmptyResult();
-//            return RedirectToAction("Index", "Home", new { page = 1, sortBy = Name});
+            var movie = new Movie() {Name = "Die Hard"};
+
+            var customers = new List<Customer>
+            {
+                new Customer() {Name = "Customer 1"},
+                new Customer() {Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+
+//          return Content("Hello World!");
+//          return HttpNotFound();
+//          return new EmptyResult();
+//          return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
         }
+
+        public ActionResult ViewMovies()
+        {
+            var listMovies = new List<Movie>()
+            {
+                new Movie(){ Name = "Die Hard"},
+                new Movie(){ Name = "Seigneur des anneaux"}
+            };
+
+            var viewModel = new MoviesMovieViewModel()
+            {
+                ListMovies = listMovies
+            };
+            return View(viewModel);
+        }
+/*        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(string.Format("pageIndex={0}&sortBy{1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}:range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        } */
     }
 }
